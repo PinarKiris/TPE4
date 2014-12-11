b85e19188506934475d0b3420d9826d07d5772d1
PinarKiris <pinar.kiris@hotmail.de>
2014-12-11 10:09:33 +0100

    CrypterCeaser

--- src/hs_mannheim/ws14/tpe_uib_05/ueb4/CrypterCaesar.java
+++ src/hs_mannheim/ws14/tpe_uib_05/ueb4/CrypterCaesar.java
@@ -1,10 +1,12 @@
 package hs_mannheim.ws14.tpe_uib_05.ueb4;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.HashMap;
 
 public class CrypterCaesar implements Crypter {
 
+	private static final int i = 0;
 	// http://wiki.freitagsrunde.org/Javakurs/%C3%9Cbungsaufgaben/C%C3%A4sar-Chiffre/Musterloesung
 
 	/*
@@ -38,29 +40,79 @@ public class CrypterCaesar implements Crypter {
 	 * 
 	 * }
 	 */
+	
+	private String schluessel;
+	
+	public CrypterCaesar (String schluessel){
+		this.schluessel = schluessel;
+	}
+	
+	
+	
+	
 	@Override
 	public String encrypt(String message) throws CrypterException {
-		return null;
+		int verschiebung = ((int) this.schluessel.charAt (0)) - 64;
+		return encrypt (String(message, verschiebung));
 
 	}
 
-	@Override
-	public List<String> encrypt(List<String> messages) throws CrypterException {
+	private String String(String message, int verschiebung) {
+		message = message.toUpperCase();
+		String ergebnis = "";
+		for (int i = 0; i< message.length(); i++);
+			
+			ergebnis += (char) ((message.charAt(i) - 'A' + verschiebung) % 26 + 'A' );
+		return ergebnis;
+	}
 
-		return null;
+
+	@Override
+	public List<String> encrypt(List<String> messagesGanz) throws CrypterException {
+		List <String> ergebnis = new LinkedList<String>();
+		for (String message : messagesGanz){
+			ergebnis.add(encrypt(message));
+		}
+		
+
+		return ergebnis;
 	}
 
 	@Override
 	public String decrypt(String cypherText) throws CrypterException {
+		int verschiebung = (int) this.schluessel.charAt(0) + 64;
 
-		return null;
+		return decrypt (String(cypherText, -verschiebung));
+	}
+	
+	
+	@SuppressWarnings("unused")
+	private String decrypt(String text, int verschiebung) {
+		text = text.toUpperCase();
+		String ergebnis = "";
+		for (int i = 0; i< text.length(); i++);
+			
+			char zeichen  = (char) ((text.charAt(i) - 'A' + verschiebung) % 26 + 'A' );
+			if (zeichen < 65){
+				ergebnis += (char) (zeichen +26);
+			}else{
+				ergebnis += (char) zeichen;
+				
+			}
+				
+		return ergebnis;
 	}
 
-	@Override
-	public List<String> decrypt(List<String> cypherTexte)
-			throws CrypterException {
+		
 
-		return null;
+	@Override
+	public List<String> decrypt(List<String> cypherTexteGanz)
+			throws CrypterException {
+		List <String> ergebnis = new LinkedList<String>();
+		for (String text : cypherTexteGanz){
+			ergebnis.add(encrypt(text));
+		}
+		return ergebnis;
 	}
 
 }
