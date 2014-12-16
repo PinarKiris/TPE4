6ee3e6d0ebad4bdbb29e1b7e0dd966ef34289fd3
Crystallize5000 <eraser5000@gmx.de>
2014-12-16 17:03:18 +0100

    Nullchiffre completed + changes at fac

--- src/hs_mannheim/ws14/tpe_uib_05/ueb4/CrypterNull.java
+++ src/hs_mannheim/ws14/tpe_uib_05/ueb4/CrypterNull.java
@@ -3,6 +3,8 @@ package hs_mannheim.ws14.tpe_uib_05.ueb4;
 import java.util.List;
 
 /**
+ * Diese Klasse CrypterNull verschluesselt den Text nicht. Die Ausgabe der
+ * Methoden entspricht der Eingabe.
  * 
  * @author Dennis Keßler 1326697
  * @author Pınar Kırış 1332722
@@ -11,35 +13,29 @@ import java.util.List;
 
 public class CrypterNull implements Crypter {
 
-	 /**
-     * Konstruktor zum erstellen einer Nullchiffre Verschluesselung
-     */
-    public CrypterNull(){
-        super();
-    }
-    
-    @Override
-    public String encrypt(String message) throws CrypterException {
-        //loescht alle ungueltigen Zeichn und gibt den String zurueck
-        return CrypterTools.deleteForbiddenCharakters(message);
-    }
+	@Override
+	public String encrypt(String message) throws CrypterException {
+		// Gibt die Nachricht unveraendert zurueck
+		return message;
+	}
 
-    @Override
-    public List<String> encrypt(List<String> messages) throws CrypterException {
-        for(String thisString : messages){
-            thisString = encrypt(thisString);
-        }
-        return messages;
-    }
+	@Override
+	public List<String> encrypt(List<String> messages) throws CrypterException {
+		// Gibt eine Liste an Nachrichten unveraendert zurueck
+		return messages;
+	}
 
-    @Override
-    public String decrypt(String cypherText) throws CrypterException {
-        return encrypt(cypherText);
-    }
+	@Override
+	public String decrypt(String cypherText) throws CrypterException {
+		// Gibt die Nachricht unveraendert zurueck
+		return cypherText;
+	}
 
-    @Override
-    public List<String> decrypt(List<String> cypherTexte) throws CrypterException {
-        return encrypt(cypherTexte);
-    }
+	@Override
+	public List<String> decrypt(List<String> cypherTexte)
+			throws CrypterException {
+		// Gibt eine Liste an Nachrichten unveraendert zurueck
+		return cypherTexte;
+	}
 
 }
